01be83734c3d80f041e76379f693ab109b6d8950
qianqing <qian.qing@aliyun.com>
2017-03-27 15:24:24 +0800

    增加mapMutations示例

--- template/src/views/Home.vue
+++ template/src/views/Home.vue
@@ -8,14 +8,24 @@
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
-    return {
-      msg: '少年，把你的代码撸起来，不要停！！！'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-    };
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-};
+  import {mapMutations} from 'vuex';
+  
+  export default {
+    name: 'hello',
+    data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
+      return {
+        msg: '少年，把你的代码撸起来，不要停！！！'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+      };
+    },
+    methods: {
+      ...mapMutations{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}([
+        'UPDATE_CUSTOMER_INFO',
+      ]),
+      getCustomerInfo{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
+        this.UPDATE_CUSTOMER_INFO({});
+      }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
