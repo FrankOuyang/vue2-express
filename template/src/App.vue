2637be03f6ed0bd8f94b1e18a7c403f322da58c0
qianqing <qian.qing@aliyun.com>
2017-03-27 19:50:22 +0800

    fix bug

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img src="./assets/images/logo.png">
-    <router-view name="router-fade" mode="out-in"></router-view>
+    <transition name="router-fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
