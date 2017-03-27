3d7ac3e5dec687aba1aaee58b11ac3f242c971d5
qianqing <qian.qing@aliyun.com>
2017-03-27 16:38:04 +0800

    增加css过渡效果

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/images/logo.png">
-    <router-view></router-view>
+    <router-view name="router-fade" mode="out-in"></router-view>
   </div>
 </template>
 
@@ -12,12 +12,25 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .router-fade-enter-active, .router-fade-leave-active {
+    transition: opacity .3s;
+  }
+  .router-fade-enter, .router-fade-leave-active {
+    opacity: 0;
+  }
+  .router-slid-enter-active, .router-slid-leave-active {
+    transition: all .4s;
+  }
+  .router-slid-enter, .router-slid-leave-active {
+    transform: translate3d(2rem, 0, 0);
+    opacity: 0;
+  }
 </style>
