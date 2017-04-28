c42ce163bbf9c02d0470d3d0a1adbf80db20dd73
qianqing <qian.qing@aliyun.com>
2017-04-28 14:59:10 +0800

    add api

--- template/server/logger.js
+++ template/server/logger.js
@@ -31,6 +31,13 @@ log4js.configure({
       'backups': 10
     },
     {
+      'category': 'api',
+      'type': 'dateFile',
+      'filename': __dirname + '/logs/api.log',
+      'pattern': '-yyyy-MM-dd',
+      'backups': 10
+    },
+    {
       'type': 'console'
     }]
 });
@@ -42,16 +49,19 @@ if (environment === 'production') {
   log4js.getLogger('system').setLevel('INFO');
   log4js.getLogger('error').setLevel('INFO');
   log4js.getLogger('oss').setLevel('INFO');
+  log4js.getLogger('api').setLevel('INFO');
 } else {
   log4js.getLogger('access').setLevel('DEBUG');
   log4js.getLogger('system').setLevel('DEBUG');
   log4js.getLogger('error').setLevel('DEBUG');
   log4js.getLogger('oss').setLevel('DEBUG');
+  log4js.getLogger('api').setLevel('DEBUG');
 }
 
 module.exports = {
   access: log4js.getLogger('access'),
   system: log4js.getLogger('system'),
   oss: log4js.getLogger('oss'),
+  api: log4js.getLogger('api'),
   express: log4js.connectLogger(log4js.getLogger('access'), {level: log4js.levels.INFO, format:':remote-addr :method :url :status'}),
 };
